103be934a86e486a6770e96c04c5fe799cf35ad7
OlDashkevych <dashkevich672@gmail.com>
2019-12-14 00:07:21 +0200

    Updates

---
+++ precache-manifest.961a71a51d7345b87dc6c2b7c183046e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb84b9785684432b52a6ffae0697d7c",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "1205393407d2a3060370",
+    "url": "/goit-react-hw-02-reader/static/css/main.6866a56c.chunk.css"
+  },
+  {
+    "revision": "1cc7b1523e523c0b9ad9",
+    "url": "/goit-react-hw-02-reader/static/js/2.450346d6.chunk.js"
+  },
+  {
+    "revision": "1205393407d2a3060370",
+    "url": "/goit-react-hw-02-reader/static/js/main.8f9084fe.chunk.js"
+  },
+  {
+    "revision": "5b4ed302ac67297a9749",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.f10f73ea.js"
+  }
+]);
